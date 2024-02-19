3d5d4c4fa9c461cbac4220603f06aed468388df6
LalisaYodmanee <fromwhas@gmail.com>
2024-02-19 23:39:45 +0700

    put book-id

--- rest_api/server.js
+++ rest_api/server.js
@@ -18,6 +18,10 @@ app.post('/books', (req, res) => {
     books.push(req.body)
     res.status(201).json(req.body)
 })
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
+})
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
